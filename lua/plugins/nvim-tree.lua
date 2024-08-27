a7f0e9eaf35e69343d07ec302b47899f2672272a
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2024-08-27 12:42:17 +0200

    fixed: nvim tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,9 +4,10 @@ Arbol de carpeta
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  cmd = {"NvimTreeToggle", "NvimTreeOpen", "NvimTreeClose"},
-  keys = {
-    { "<leader>t", ":NvimTreeToggle<cr>", mode = "n", desc = "tree toggle"}
-  },
-  opts = {}
+  opts = {},
+  config = function (plugin)
+    require("nvim-tree").setup(plugin.opts)
+
+    vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<cr>", { desc = "tree toggle"})
+  end
 }
