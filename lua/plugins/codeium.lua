d6be8de02b3295448e4e012e2cc2a827b0017725
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2024-08-27 14:06:56 +0200

    fixed keymap codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -8,6 +8,6 @@ return {
     vim.cmd("call CodeiumDisable()")
 
     vim.keymap.set("n", "<leader>ia", ":CodeiumToggle<cr>", {desc = "Codeium toggle"})
-    vim.keymap.set("n", "<leader>ia", ":CodeiumToggle<cr>", {desc = "Codeium toggle"})
+    vim.keymap.set("n", "<leader>ai", ":CodeiumToggle<cr>", {desc = "Codeium toggle"})
   end
 }
